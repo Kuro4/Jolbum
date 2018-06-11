2227fdb63b60a4f73c78928d3d0b75142566f2cf
okada <yoyh9ab@outlook.jp>
2018-06-12 08:27:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Jolbum/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Jolbum.Views;
+using System.Windows;
+using Prism.Modularity;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace Jolbum
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            var moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(YOUR_MODULE));
+        }
+    }
+}
